4abdf9256a15c2ec62b6a67aa49f5aac506fdb8b
Itheima_YM <Itheima_YM@itcast.cn>
2019-09-15 15:39:06 +0800

    1st commit Hello.java

---
+++ git_test_project/src/com/itheima/test/Hello.java
@@ -0,0 +1,14 @@
+package com.itheima.test;
+
+/**
+ * @author 廖艺铭
+ * @Title: Hello
+ * @ProjectName git_test_project
+ * @Description: TODO
+ * @date 2019/6/24
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
